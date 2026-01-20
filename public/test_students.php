9a0c8c14a00ae6e4e8612ec9d0097acf0cc62c39
Bense <115864904+Bensemwikhali@users.noreply.github.com>
2026-01-20 13:04:19 +0300

    Display students using php arrays and loops

---
+++ public/test_students.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        <title>Students List</title>
+    </head>
+    <body>
+        <h1>Students Information</h1>
+        <table border="1">
+            <tr>
+                <th>Name</th>
+                <th>Adm No</th>
+                <th>Grade</th>
+            </tr>
+            <?php
+            $students=[
+                [    'name'=>'Ashley Okinyo',
+                'age'=>20,
+                'grade'=>13,
+                'adm_no'=>3120,
+            ],
+            [
+               'name'=>'Vera Auma',
+                'age'=>21,
+                'grade'=>14,
+                'adm_no'=>3121, 
+            ],
+            [
+                'name'=>'John Kamau',
+                 'age'=>14,
+                 'grade'=>7,
+                 'adm_no'=>3122,
+            ],
+            [
+                'name'=>'Mary Wafula',
+                 'age'=>12,
+                 'grade'=>6,
+                 'adm_no'=>3123,
+            ],
+            ];
+            foreach($students as $student){
+                echo "<tr><td>".$student["name"]."</td><td>".$student["adm_no"]."</td><td>".$student["grade"]."</td></tr>";
+            }
+            ?>
+        </table>
+    </body>
+</html>
