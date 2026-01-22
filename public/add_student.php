815c5114322fae4c6c288dee164169947a374373
Bense <115864904+Bensemwikhali@users.noreply.github.com>
2026-01-22 12:55:51 +0300

    commited a student registration form using POST method

---
+++ public/add_student.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student registration form</title>
+</head>
+<body>
+    <h1>STUDENT REGISTRATION FORM</h1>
+    <form action="">
+        <label for="username">Username</label>
+        <input type="text" id="username" name="username" placeholder="Enter your full name">
+
+        <label for="adm_no">Admission number</label>
+        <input type="text" id="adm_no"name="adm_no" placeholder="Enter your admission number">
+
+        <label for="grade">Grade</label>
+        <input type="text"id=grade name="grade" placeholder="Enter your grade">
+        <button type="submit">Submit</button>
+
+
+
+
+
+    </form>
+</body>
+</html>
