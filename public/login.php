caa2a821988c78a796478c82a9bb0e8f90096c41
Bense <115864904+Bensemwikhali@users.noreply.github.com>
2026-01-29 11:31:08 +0300

    displays flash messages on the login page

--- public/login.php
+++ public/login.php
@@ -6,6 +6,21 @@
     <title>Login</title>
 </head>
 <body>
+    <?php
+require_once "helpers/flash.php";
+
+$error = get_flash('error');
+$success = get_flash('success');
+?>
+
+<?php if ($error): ?>
+    <p style="color:red;"><?= htmlspecialchars($error) ?></p>
+<?php endif; ?>
+
+<?php if ($success): ?>
+    <p style="color:green;"><?= htmlspecialchars($success) ?></p>
+<?php endif; ?>
+
     <h1>Login</h1>
 
     <form action="process_login.php" method="POST">
